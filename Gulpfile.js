f4731d80970e4f74c7082aa9b0c6b80cfcc947f9
pindakaas <dingen2008@gmail.com>
2015-09-28 08:43:00 +1000

    browsersync

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,8 +6,29 @@ gulp.task('hello', function() {
 
 var sass = require('gulp-sass');
 
-gulp.task('sass', function(){
-    return gulp.src('app/scss/styles.scss')
-        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+var browserSync = require('browser-sync');
+
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        }
+    })
+})
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sass())
         .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    // Other watchers
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
 });
